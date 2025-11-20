ac91bb74616d199371446cd14ca52832f3ef3d6b
anastasiiapohorila <anastasiia@iagon.com>
2025-11-20 11:22:51 +0400

    add screenshot testing

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
     stories: ['../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-webpack5-compiler-swc'],
+    addons: ['@storybook/addon-webpack5-compiler-swc', '@chromatic-com/storybook'],
     framework: {
         name: '@storybook/react-webpack5',
         options: {},
